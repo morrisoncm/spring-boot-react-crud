f6e5304edeccebfd3c7f51105af242c265fe4e9b
cmorrison <cmorrison@tsys.com>
2022-08-30 14:15:58 -0400

    Training

--- src/test/java/com/demo/springandreact/test/ContextTest.java
+++ src/test/java/com/demo/springandreact/test/ContextTest.java
@@ -3,8 +3,10 @@ package com.demo.springandreact.test;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import com.demo.springandreact.controller.ClientController;
 
+@SpringBootTest
 class ContextTest {
 
   @Autowired
