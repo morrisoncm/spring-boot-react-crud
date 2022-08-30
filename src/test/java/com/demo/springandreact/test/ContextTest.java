89952339f6d3a26ab3c65486bef58beb816844b2
cmorrison <cmorrison@tsys.com>
2022-08-30 13:50:55 -0400

    Training

--- src/test/java/com/demo/springandreact/test/ContextTest.java
+++ src/test/java/com/demo/springandreact/test/ContextTest.java
@@ -1,5 +1,17 @@
 package com.demo.springandreact.test;
 
-public class ContextTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.demo.springandreact.controller.ClientController;
 
+class ContextTest {
+ 
+  @Autowired
+  private ClientController controller;
+  
+  @Test
+  void contextLoads() throws Exception {
+      assertThat(controller).isNotNull();
+  }
 }
