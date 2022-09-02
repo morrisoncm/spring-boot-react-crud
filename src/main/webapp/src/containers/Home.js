81430af9a42377f1914238def19d44560fbb4296
cmorrison <cmorrison@tsys.com>
2022-09-02 10:23:42 -0400

    Training

---
+++ src/main/webapp/src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import AppNavbar from './AppNavbar';
+import { Link } from 'react-router-dom';
+import { Button, Container } from 'reactstrap';
+
+class Home extends Component {
+	render() {
+		return (
+			<p>needs completed</p>
+		);
+	}
+}
+
+export default Home;
