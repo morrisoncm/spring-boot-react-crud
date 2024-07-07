5f684212893f333e60b87f7d160d2b9bccc46c76
Clinton Morrison <morrison.clinton@gmail.com>
2024-07-07 00:28:16 -0400

    jakarta package

--- src/main/java/com/demo/springandreact/domain/entity/Client.java
+++ src/main/java/com/demo/springandreact/domain/entity/Client.java
@@ -1,12 +1,14 @@
 package com.demo.springandreact.domain.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
